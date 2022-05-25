5406a2f56bf3b962f323f346ba63398899263bf2
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2022-05-25 18:03:07 +0200

    Fixed pdf-view mode page count

--- nano-modeline.el
+++ nano-modeline.el
@@ -906,7 +906,7 @@ depending on the version of mu4e."
     (mode-name   (nano-modeline-mode-name))
     (branch      (nano-modeline-vc-branch))
     (page-number (concat
-              (number-to-string (pdf-view-current-page)) "/"
+              (number-to-string (image-mode-window-get 'page)) "/"
               (or (ignore-errors
                 (number-to-string (pdf-cache-number-of-pages)))
               "???"))))
